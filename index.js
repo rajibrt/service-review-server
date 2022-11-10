609cbb0c1abb0e5dfc1ec451010f4fa27fd232c4
rajibrt <rajibrt@gmail.com>
2022-11-10 20:00:24 +0600

    401 and 403 updated

--- index.js
+++ index.js
@@ -106,6 +106,9 @@ async function run() {
         });
 
         app.get('/myreviews', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;;
+            if (decoded.email !== req.query.email) { }
+            res.status(403).send({ message: 'unauthorized access' })
             let query = {};
             if (req.query.email) {
                 query = {
