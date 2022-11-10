9b5d254163d6f5eef14203e911007a1912ae092f
rajibrt <rajibrt@gmail.com>
2022-11-10 19:43:57 +0600

    JWT token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -13,10 +14,32 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wx23zka.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: `unauthorized access` });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: `Forbidden access` });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('trust').collection('services');
         const reviewCollection = client.db('trust').collection('reviews');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token });
+
+        });
 
         app.get('/services', async (req, res) => {
             const query = {};
@@ -82,7 +105,7 @@ async function run() {
             res.send(reviews);
         });
 
-        app.get('/myreviews', async (req, res) => {
+        app.get('/myreviews', verifyJWT, async (req, res) => {
             let query = {};
             if (req.query.email) {
                 query = {
@@ -99,6 +122,16 @@ async function run() {
             const result = await reviewCollection.deleteOne(query);
             res.send(result);
         });
+
+        // app.patch('/reviews/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //             const query = { _id: ObjectId(id) };
+        //     const updatedDoc = {
+        //         Sset:{
+
+        //         }
+        //     }
+        // })
 
     }
     finally {
